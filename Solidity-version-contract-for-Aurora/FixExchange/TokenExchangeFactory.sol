872b897ab9cb905758ba7cbd0c1f8aa0aaf94137
Dickensonn <dickensonn@163.com>
2022-02-18 14:06:42 +0800

    tokenExchangeFactory

--- Solidity-version-contract-for-Aurora/FixExchange/TokenExchangeFactory.sol
+++ Solidity-version-contract-for-Aurora/FixExchange/TokenExchangeFactory.sol
@@ -8,13 +8,17 @@ contract tokenExchangeFactory{
     uint public time;
     uint public releasePeriod;
     uint public closePeriod;
+    uint public  chooseDay;
+    uint public extractableRate;
+    uint public circulation1;
+    uint public index;
+    uint public index2;
+    uint public index3;
+    //pool[] public list4;
     mapping(uint=>info)fixedExchangeInfo;
     struct info{
         string information;
         uint rate;
-        // uint time;
-        // uint releasePeriod;
-        // uint closePeriod;
     }
     string public appointExchangeInfo;
     struct Token1{
@@ -38,27 +42,54 @@ contract tokenExchangeFactory{
         //uint id;
         uint amount;
         address user;
-        uint time;
+        //uint time;
+        uint depositTime;
     }
     userInfo []public list3;
-    uint public rate ;
-    event SetExchangeRate(uint rates,uint _releasePeriod,uint _closePeriod);
-    event AddToken(address _token1,string  _name1, string _symbol1,uint _price1,uint _circulation1,address _token2,string  _name2, string  _symbol2,uint _price2,uint _circulation2);
-    event Deposit(address token2,uint amount);
-    //event ExchangeToken(uint amount);
-    constructor(string memory name ,string memory logo,string memory info) public {
-        factoryName = name;
-        factoryLogo=logo;
-        factoryIntroduction=info;
+    struct pool{
+        uint index;
+        string  name1;
+        string  name2;
+        address owner;
+        address contracts;
     }
-   
+    pool []public list4;
+    struct factoryInfo{
+        string factoryName;
+        string factoryLogo;
+        string factoryIntroduction;
+    }
+    factoryInfo [] public list5;
+    
+     struct userInfo2{
+        //uint id;
+        uint amount;
+        address user;
+       // uint time;
+        uint withdrawalTime;
+    }
+    userInfo2 []public list6;
+    
+    uint public rate;
 
-    function addToken(address _token1,string memory _name1, string memory _symbol1,uint _price1,uint _circulation1,address _token2,string memory _name2, string memory _symbol2,uint _price2,uint _circulation2)public{
+    function cteateFactory(string memory name ,string memory logo,string memory info)public{
+        index=index+1;
+        factoryInfo memory info=factoryInfo({
+        factoryName :name,
+        factoryLogo:logo,
+        factoryIntroduction:info
+        });
+        list5.push(info);
+
+    }
+
+    function addToken(address _token1,string memory _name1, string memory _symbol1,uint _circulation1,address _token2,string memory _name2, string memory _symbol2,uint _circulation2)public{
+        circulation1=_circulation1;
         Token1 memory token1Info=Token1({
             token:_token1,
             name:_name1,
             symbol:_symbol1,
-            price:_price1,
+            price:100,
             circulation:_circulation1
         });
         list1.push(token1Info);
@@ -66,79 +97,172 @@ contract tokenExchangeFactory{
             token:_token2,
             name:_name2,
             symbol:_symbol2,
-            price:_price2,
+            price:50,
             circulation:_circulation2
         });
         list2.push(token2Info);
-        emit AddToken(_token1,_name1,_symbol1,_price1,_circulation1,_token2,_name2,_symbol2,_price2,_circulation2);
+        index=index+1;
+        pool memory poolInfo=pool({
+            index:index,
+            name1:_name1,
+            name2:_name2,
+            owner:msg.sender,
+            contracts:address(this)
+        });
+        list4.push(poolInfo);
     }
 
-    function setExchangeRate(uint number,uint rates,uint way ,uint price1,uint price2,uint _releasePeriod,uint _closePeriod )public{
+    function setExchangeRate(uint number,uint rates,uint way ,uint _releasePeriod,uint _closePeriod ,uint chooseDay)public{
         closePeriod=_closePeriod*2592000;
         releasePeriod=_releasePeriod*2592000;
+        uint32 blockTime=uint32(block.timestamp % 2 ** 32);
         if (number==1){
             fixedExchangeInfo[number].information="fixed exchange rate";
             fixedExchangeInfo[number].rate=rates;
             rate=rates;
+            if (chooseDay==1){
+                extractableRate=86400/releasePeriod;
+               //extractableRate=((blockTime-time-closePeriod)/86400)/releasePeriod;
+            }
+            if(chooseDay==2){
+                extractableRate=(86400/releasePeriod)*30;
+                //extractableRate=((blockTime-time-closePeriod)/releasePeriod)*30;
+            }
+            if(chooseDay==3){
+                extractableRate=1;
+            }
         }
         else if(number==2){
             appointExchangeInfo="the exchange rate of agreed amount";  
             if(way==1){
-                rate =list1[0].price/list2[0].price;
+                rate =list1[index-1].price/list2[0].price;
+                 if (chooseDay==1){
+                    extractableRate=86400/releasePeriod;
+                     //extractableRate=((blockTime-time-closePeriod)/86400)/releasePeriod;
+                 }
+                if(chooseDay==2){
+                    extractableRate=(86400/releasePeriod)*30;
+                //extractableRate=((blockTime-time-closePeriod)/releasePeriod)*30;
+                }
+                if(chooseDay==3){
+                    extractableRate=1;
+                }
             }
             if(way==2){
-                rate=list1[0].price/price2;
+                rate=list1[index-1].price/50;
+                if (chooseDay==1){
+                    extractableRate=86400/releasePeriod;
+                 }
+                if(chooseDay==2){
+                    extractableRate=(86400/releasePeriod)*30;
+                }
+                if(chooseDay==3){
+                    extractableRate=1;
+                }
             }
             if(way==3){
-                rate=price1/list2[0].price;
+                rate=100/list2[index-1].price;
+                if (chooseDay==1){
+                    extractableRate=86400/releasePeriod;
+                 }
+                if(chooseDay==2){
+                    extractableRate=(86400/releasePeriod)*30;
+                }
+                if(chooseDay==3){
+                    extractableRate=1;
+                }
             }
             if(way==4){
-                rate=price1/price2;
+                rate=100/50;
+                if (chooseDay==1){
+                    extractableRate=86400/releasePeriod;
+                     //extractableRate=((blockTime-time-closePeriod)/86400)/releasePeriod;
+                 }
+                if(chooseDay==2){
+                    extractableRate=(86400/releasePeriod)*30;
+                //extractableRate=((blockTime-time-closePeriod)/releasePeriod)*30;
+                }
+                if(chooseDay==3){
+                    extractableRate=1;
+                }
             }
         }
-        emit SetExchangeRate(rates,_releasePeriod,_closePeriod);
+        //emit SetExchangeRate(rates,_releasePeriod,_closePeriod);
     }
     function deposit(address token2,uint amount)public{
+        index2=index2+1;
         uint32 blockTime=uint32(block.timestamp % 2 ** 32);
         time=blockTime;
-        //uint userBalance=IERC20(list2[1].token).balanceOf(msg.sender);
-        require(amount<list2[0].circulation,"over circulation");
-        //IERC20(list2[0].token).transfer(address(this),amount);
+        require(amount<list2[index2-1].circulation,"over circulation");
         TransferHelper.safeTransferFrom(token2 ,msg.sender,address(this), amount);
         userInfo memory userinfo=userInfo({
             //id: id,
             amount:amount,
             user:msg.sender,
-            time:blockTime
+            //time:blockTime,
+            depositTime:time
         });
         list3.push(userinfo);
-        emit Deposit(token2,amount);
+        //emit Deposit(token2,amount);
     }
     function exchangeToken(address token,uint number,uint amount )public{
+        index3=index3+1;
         //require(msg.sender==list3[0].user,"does not comply with exchange rules");
         uint32 blockTime=uint32(block.timestamp % 2 ** 32);
         require(blockTime>time+closePeriod,"Complete sealing period can not be extracted");
-        require(amount<=list3[0].amount);
+        //require(amount<=list3[index2-1].amount);
+        list3[index2-1].amount=list3[index2-1].amount-amount;
         if(number==1){
-            //TransferHelper.safeTransferFrom(token,address(this),msg.sender,amount);
             IERC20(token).transfer(msg.sender,amount);
-            list3[0].amount=list3[0].amount-amount;
+            list3[index2-1].amount-amount;
+            userInfo2 memory userinfo=userInfo2({
+            amount:list3[index2-1].amount,
+            user:msg.sender,
+            //time:blockTime,
+            withdrawalTime:blockTime
+        });
+        list6.push(userinfo);
+           
         }
-            if (number==2){
-              uint amounts= list3[1].amount*(blockTime-time-closePeriod)/releasePeriod;
-              require(amount<=amounts,"excess release limit");
-                IERC20(token).transfer(msg.sender,amount);
-                //TransferHelper.safeTransferFrom(token,address(this),msg.sender,amount);
-                list3[0].amount=list3[0].amount-amount;
-             }
-             else if(number==3){
-                uint amounts= list3[1].amount*((blockTime-time-closePeriod)/releasePeriod)*30;
-                require(amount<=amounts,"excess release limit");
-                IERC20(token).transfer(msg.sender,amount);
-                TransferHelper.safeTransferFrom(token,address(this),msg.sender,amount);
-                list3[0].amount=list3[0].amount-amount;
-             }
-             //emit ExchangeToken(list3[0].amount);
+        if (number==2){
+            uint amounts= list3[index-1].amount*(((blockTime-time-closePeriod)/86400)*extractableRate)/rate;
+            require(amount<=amounts,"excess release limit");
+            IERC20(token).transfer(msg.sender,amount);
+            userInfo2 memory userinfo=userInfo2({
+            amount:list3[index2-1].amount-amount,
+            user:msg.sender,
+            //time:blockTime,
+            withdrawalTime:blockTime
+        });
+        list6.push(userinfo);
+        }
+    }
+    
+    function balanceOfToken1()public view returns(uint){
+        return circulation1;
+        
+    }
+    
+    function balanceOfToken2()public view returns(uint){
+        return list3[0].amount;
+    }
+    
+    function Token1Info()public view returns(Token1 memory){
+        return list1[0];
+    }
+    
+     function Token2Info()public view returns(Token2 memory){
+        return list2[0];
+    }
+    function userinfo()public view returns(userInfo memory){
+        return list3[0];
+    }
+    
+    function poollist2()public view returns(pool memory){
+        for(uint i=0;i<index;i++){
+            return list4[i];
+        }
+        
     }
 
 }
--- Solidity-version-contract-for-Aurora/README.md
+++ Solidity-version-contract-for-Aurora/README.md
@@ -1,1 +1,57 @@
 Solidity-version-contract-for-Aurora
+## ##Dependencies##
+
+- Linux , Mac or Windows
+
+## test-net-contract
+
+test-network:aurora
+
+## Contract introduction
+
+-TokenExchangeFactory:This contract is used to store the exchange pool, where tokens are exchanged. 
+
+-RBD:Test the tokens;
+
+-RBE:Test the tokens;
+
+-TransferHelper:Transfer tools;
+
+## **Installing**
+
+Please make sure you have the corresponding versions of npm :
+
+```bash
+npm -v
+```
+
+```bash
+npm install npm@latest -g
+```
+
+```bash
+npm install @openzeppelin/contracts
+```
+
+```bash
+git clone https://github.com/RainbowDAO/07-Near-MetaBUILD-DAO-Fixed-Exchange.git
+cd 07-Near-MetaBUILD-DAO-Fixed-Exchange/Solidity-version-contract-for-Aurora
+npm install
+```
+## Testing
+
+First of all you need to change the network:
+
+https://aurora.dev/start
+
+Then, Enter the webpage and select the Aurora Network test network and click Add network
+
+contracts address
+
+TokenExchangeFactory:0x748c8E1B67C3306fE7B0aFC0Bca935438914E3ce
+
+RBD:0x31b1D5ffA27Eb033dc2F609977416314F6d8dCD9
+
+RBE:0x260b623a5adbcA8A456F777edA50637AA1aBFC17
+
+
